1118e879c59416e8e2d3a55e5dbc6e8b25cb3d74
folkersm <folkersm@gmail.com>
2023-05-29 19:33:10 -0500

    added functions for rendering board

--- app.js
+++ app.js
@@ -18,6 +18,23 @@ canvas.height = boardPixelSize;
 // Initialize the game board as a 2D array
 const gameBoard = new Array(boardSize).fill(null).map(() => new Array(boardSize).fill(null));
 
+//utility functions:
+function arraysEqual(a, b) {
+  if (a === b) return true;
+  if (a == null || b == null) return false;
+  if (a.length !== b.length) return false;
+
+  // If you don't care about the order of the elements inside
+  // the array, you should sort both arrays here.
+  // Please note that calling sort on an array will modify that array.
+  // you might want to clone your array first.
+
+  for (var i = 0; i < a.length; ++i) {
+    if (a[i] !== b[i]) return false;
+  }
+  return true;
+}
+
 // Define the Person class
 class Person {
   constructor(player, startingX, startingY) {
@@ -57,6 +74,34 @@ class Person {
   }
 }
 
+
+
+const direction_list = [[-1,-1],[0,-1],[1,-1],[1,0],[1,1],[0,1],[-1,1],[-1,0]]
+function rotate_fortyfive(direction, orientation) {// orientation = 1 means clockwise, =-1 means counterclock
+	//find index in list that your direction sits
+	found_direction = false
+	index = 0
+	while (!found_direction) {
+		if (arraysEqual(direction_list[index], direction) || index ==7) {
+			found_direction = true
+		}
+		else index+=1
+	}
+	if (orientation == 1) {
+		if (index != 7) {
+			return direction_list[index+1]
+		}
+		else {return [-1,-1]}
+	}
+	else {
+		if (index != 0) {
+			return direction_list[index-1]
+		}
+		else {return [-1,0]}
+	} 
+}
+
+
 // Client-side function to update player data in HTML element
 function updatePlayerData(playerData) {
   const playerElement = document.getElementById("playerData");
@@ -65,7 +110,7 @@ function updatePlayerData(playerData) {
   playerElement.innerHTML = `
     <p>ID: ${playerData.id}</p>
     <p>Name: ${playerData.name}</p>
-    <p>Score: ${playerData.score}</p>
+    <p>Ships: ${playerData.shipCount}</p>
     <p>Level: ${playerData.level}</p>
   `;
 }
@@ -74,8 +119,30 @@ socket.on("playerData", function(playerData) {
   updatePlayerData(playerData);
 });
 
+
+function render_piece(x,y,direction) {
+	if (Math.abs(direction[0]) + Math.abs(direction[1]) == 1) {
+		clock_tilt = rotate_fortyfive(rotate_fortyfive(direction,1),1);
+		counter_tilt = rotate_fortyfive(rotate_fortyfive(direction,-1),-1);
+	}
+	else {
+		clock_tilt = rotate_fortyfive(direction,1);
+		counter_tilt = rotate_fortyfive(direction,-1);
+	}
+	ctx.beginPath();
+	ctx.moveTo((x-.5)*tileSize+(-.5*direction[0])*tileSize*.5,(y-.5)*tileSize+(-.5*direction[1])*tileSize*.5)
+	ctx.lineTo((x-.5)*tileSize+(.5*direction[0])*tileSize*.5,(y-.5)*tileSize+(.5*direction[1])*tileSize*.5)
+	ctx.moveTo((x-.5)*tileSize+(.5*clock_tilt[0])*tileSize*.5,(y-.5)*tileSize+(.5*clock_tilt[1])*tileSize*.5)
+	ctx.lineTo((x-.5)*tileSize+(.5*direction[0])*tileSize*.5,(y-.5)*tileSize+(.5*direction[1])*tileSize*.5)
+	ctx.moveTo((x-.5)*tileSize+(.5*counter_tilt[0])*tileSize*.5,(y-.5)*tileSize+(.5*counter_tilt[1])*tileSize*.5)
+	ctx.lineTo((x-.5)*tileSize+(.5*direction[0])*tileSize*.5,(y-.5)*tileSize+(.5*direction[1])*tileSize*.5)
+	ctx.srokeStyle = 'black'
+	ctx.lineWidth = 2
+	ctx.stroke();
+}
+
 // Draw the game board
-function renderBoard() {
+function renderBoard(redList,blueList,placeList) {
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
 	for(let y = 0; y < boardSize; y++) { 
 		for(let x = 0; x < boardSize; x++) {
@@ -97,6 +164,38 @@ function renderBoard() {
 }
 renderBoard()
 
+// function locate_ships() {
+	// pieces_list = []
+	// for (let i = 0; i < boardSize; i++) {
+		// for (let j = 0; j < boardSize; j++) {
+			// if ((gameBoard[i][j].toLowerCase() == gameBoard[i][j]) == (playerData.ID == 'r')) {
+				// pieces_list.push([i,j])
+			// }
+		// }
+	// }
+// }
+
+// function update_board(moves) {
+	// for (move in moves) {
+		// gameBoard[move[0]][move[1]] = move[2]
+	// }
+	
+// }
+
+var place_ship = false;
+var rotate_piece = false;
+var place_list = []
+var focus_location = []
+function display_selection_tiles(tiles) {
+	place_list = tiles
+	for (let i = 0; i < tiles.length; i++) {
+		ctx.beginPath()
+		ctx.fillStyle = "green"
+		ctx.fillRect(tiles[i][0]*tileSize,tiles[i][1]*tileSize,tileSize,tileSize)
+		ctx.stroke();
+	}
+}
+display_selection_tiles([[1,1]])
 canvas.addEventListener('click', function(event) {
 	// Calculate the clicked tile's coordinates
 	const rect = canvas.getBoundingClientRect();
@@ -104,7 +203,11 @@ canvas.addEventListener('click', function(event) {
 	const y = event.clientY - rect.top;
 	const tileX = Math.floor(x / tileSize);
 	const tileY = Math.floor(y / tileSize);
-	console.log('hello')
+	if (place_ship && tiles.includes([x,y])){
+		render_piece(x,y,[1,1])
+		rotate_piece = true
+		focus_location = [x,y,[1,1]]
+	}
 	socket.emit('cellClicked', [tileX, tileY])
 	// Update and log the game state
 	// gameBoard[tileY][tileX] = 'X';
@@ -114,6 +217,25 @@ canvas.addEventListener('click', function(event) {
 	// ctx.fillStyle = 'rgba(0, 0, 255, 0.3)';
 	// ctx.fillRect(tileX * tileSize, tileY * tileSize, tileSize, tileSize);
 });
+canvas.addEventListener('keydown', function(event) {
+    switch (event.key) {
+    case "ArrowLeft":
+        if (rotate_piece) {
+			ctx.beginPath[]
+		}
+    case "ArrowRight":
+        // Right pressed
+        break;
+}
+});
+
+
+
+socket.on('placeShip', (player_moves) => {//this will signal the player client to update the board, display the board events and then show where the player can place their troops. 
+	place_ship = true
+	display_selection_tiles(player_moves)
+})
+
 
 
 socket.on('move', (elementData) => {
--- server.js
+++ server.js
@@ -33,73 +33,130 @@ currentPlayer = 'r'
 const templateNewBaord = new Array(boardSize).fill(null).map(() => new Array(boardSize).fill(null));
 
 
-xmax = {b:'d',c:'e',d:'f'}
 
-function transform_coordinates(x,y) {
-if (x=-1&&y=boardSize){ 
+function rotate_counter(direction) {//rotate cardinal direction counter clockwise two 'notches', aka 90 degrees,aka compass pieces of 8
+	newY = direction[0]*-1
+	newX = direction[1]
+	return [newX,newY]
+}
+function rotate_clock(direction) {//ditto clockwise
+	newX = direction[1]*-1
+	newY = direction[0]
+	return [newX,newY]
+}
+ 
+//if a ship's new location after the update is within the 8x8 board, then this will return the the parameters unchanged
+function transform_coords(x,y,direction) {//otherwise this will map ships that have 'fallen off the edge of the board' back onto the board and update the direction, returning new coordinates and direction. 
+if (x==-1&&y==boardSize){ 
 	x=0
 	y=boardSize-1
+	direction = [1,-1]
 }
-if (x=boardSize&&y=-1) {
-	
+if (x==boardSize&&y==-1) {
+	x=boardSize-1
+	y=0
+	direction = [-1,1]
 }
 if ((x+y)>boardSize*2-2){ 
 	x=0
 	y=0
+	direction = [-1,-1]
 }
 if (x+y==-2){ 
-	x = boardSize
-	y=boardSize
+	x = 0
+	y=0
+	direction = [1,1]
 }
 	if (x==boardSize) {
 		x=y;
 		y=0
-		
+		direction = rotate_counter(direction)
 	}
 	if (y==boardSize) {
 		y=x;
 		x=0
+		direction = rotate_clock(direction)
+		
 	}
 	if (x==-1) {
 		x=y
 		y=boardSize-1
+		direction = rotate_counter(direction)
 	}
 	if (y==-1) {
 		y=x
 		x=boardSize-1
+		direction = rotate_clock(direction)
 	}
-	return x,y
+	return [x,y,direction]
 }
 
 
-
-function update_board() {
-	var newBoard = 
-	for (let i = 0; i < boardSize; i++) {
-		for (let j = 0; j < boardSize; j++) {
-			if (gameBoard[i][j] != 0) {
-				if (gameBoard[i][j].length > 1) {
-					
-				}
-			}
-		}
-	}
-}
+// function update_board() {
+	// for (let i = 0; i < 
 
 // Server-side function to initialize player data
 function initializePlayerData(playerId) {
   // Simulating server-side data retrieval
   const playerData = {
-	player data
     id: playerId,
     name: "Player " + playerId,
-    score: 0,
-    level: 1
+    shipCount: 0,
+    level: 1,
+	shipList: [] 
   };
   
   return playerData;
 }
 
+
+
+class Ship {
+	static ship_index = 0;
+	static ship_list = [];
+	constructor(player) {
+		this.player = player;//use socket id for this
+		this.index = Ship.ship_index++;
+		Ship.ship_list.push(this)
+	}
+	place(x,y,direction) {
+		this.direction = direction
+		this.x = x
+		this.y=y
+	}
+	move() {
+		this.previousX = x
+		this.previousY = y
+		this.x +=this.direction[0]
+		this.y +=this.direction[1]
+		let temp = transform_coords(this.x,this.y,this.direction)
+		this.x = temp[0]
+		this.y = temp[1]
+		this.direction = temp[2]
+	}
+	destroy() {
+		for (let i = 0; i < ship_list.lenth; i++) {
+			if (this.index === ship_list[i].index) {
+				ship_list.splice(i,1)
+			}
+		}
+	}
+}
+function generate_moves(player_id) {
+	let list = Ship.ship_list
+	let moves = []
+	for (let i = 0; i < list; i++) {
+		if (list[i].player = player_id && [list[i].x,list[i].y] != [list[i].previousX,list[i].previousY]) {
+			moves.push([list[i].previousX,list[i].previousY])
+		}
+	}
+	return moves
+}
+
+var waiting_for_move = true;
+var waiting_for_recover = false;
+var gameState = true
+var globalShipList = []
 io.on('connection', (socket) => {
   if (playerCount < 2) {
     playerMapping[socket.id] = playerCount === 0 ? 'r' : 'b';
@@ -112,18 +169,24 @@ io.on('connection', (socket) => {
   socket.emit("playerData", playerData);
   socket.emit('boardState', { board: gameBoard, player: playerMapping[socket.id] });
 
-  socket.on('cellClicked', (data) => {
-	  x=data[0]
-	  y=data[1]
-	  console.log(`x:${x} y:${y}`)
-    if (!gameBoard[x][y] && currentPlayer === playerMapping[socket.id]) {
-	  console.log('made it to the if statement')
-      gameBoard[x][y] = currentPlayer;
-	  io.emit('move', { locationX: x, locationY: y, player: currentPlayer});
-      currentPlayer = currentPlayer === 'r' ? 'b' : 'r';
-      
-    }
-  });
+	socket.on('ship_placed', (data) => {
+		if (waiting_for_move) {
+		x=data[0]
+		y=data[1]
+		direction = data[2]
+		console.log(`x:${x} y:${y}`)
+		let moves_list = generate_moves(socket.id)
+		if (moves_list.includes([x,y])) {
+		  let temp_ship = new Ship(socket.id)
+		  temp_ship.place(x,y,)
+		  io.emit('move', { locationX: x, locationY: y, player: currentPlayer});
+		  currentPlayer = currentPlayer === 'r' ? 'b' : 'r';
+		  waiting_for_move = false;
+	  }
+	}
+	}); 
+
+  
 
   socket.on('disconnect', () => {
     playerCount--;
