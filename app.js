34925a514b1e5805a9b9abc3a98b897266a9887c
folkersm <folkersm@gmail.com>
2023-06-11 08:45:14 -0500

    huge update

--- app.js
+++ app.js
@@ -1,39 +1,70 @@
+
 const socket = io();
 const board = document.getElementById('gameBoard');
 let myPlayer;
 
 //setting up the server constants
 const canvas = document.getElementById('gameBoard');
-console.log(canvas)
+//console.log(canvas)
 const ctx = canvas.getContext('2d');
 
 //initialize board constants (thesse may want to change to match device screen size) 
 const tileSize = 50;
-const boardSize = 8;
+const boardSize = 8; 
 const boardPixelSize = tileSize * boardSize;
-
+ 
 canvas.width = boardPixelSize;
 canvas.height = boardPixelSize;
 
 // Initialize the game board as a 2D array
 const gameBoard = new Array(boardSize).fill(null).map(() => new Array(boardSize).fill(null));
+document.addEventListener("DOMContentLoaded", function() {
+var placeButton = document.createElement("button");
+placeButton.textContent = "Confirm Placement";
+	placeButton.addEventListener("click", function() {
+		if (place_ship && !arraysEqual(focus_location,[])){
+			place_ship = false;
+		rotate_piece = false;
+		socket.emit('ship_placed',[focus_location[0],focus_location[1],focus_location[2]]);
+		}   
+}); 
+var targetElement = document.getElementById("placeButton");
+targetElement.appendChild(placeButton);
+var rotateButton = document.createElement("button");
+rotateButton.textContent = "rotate piece";
+	rotateButton.addEventListener("click", function() {
+		if (place_ship && !arraysEqual(focus_location,[])){
+			respondToArrows(-1);
+		} 
+});
+var target2Element = document.getElementById("rotateButton");
+target2Element.appendChild(rotateButton);})
 
 //utility functions:
+
+function searchForArray(haystack, needle){
+  var i, j, current;
+  for(i = 0; i < haystack.length; ++i){
+    if(needle.length === haystack[i].length){
+      current = haystack[i];
+      for(j = 0; j < needle.length && needle[j] === current[j]; ++j);
+      if(j === needle.length)
+        return i;
+    }
+  }
+  return -1;
+}
+
 function arraysEqual(a, b) {
   if (a === b) return true;
   if (a == null || b == null) return false;
   if (a.length !== b.length) return false;
-
-  // If you don't care about the order of the elements inside
-  // the array, you should sort both arrays here.
-  // Please note that calling sort on an array will modify that array.
-  // you might want to clone your array first.
-
   for (var i = 0; i < a.length; ++i) {
     if (a[i] !== b[i]) return false;
   }
   return true;
 }
+
 
 // Define the Person class
 class Person {
@@ -73,14 +104,14 @@ class Person {
     return possibleMoves;
   }
 }
-
+  
 
 
 const direction_list = [[-1,-1],[0,-1],[1,-1],[1,0],[1,1],[0,1],[-1,1],[-1,0]]
 function rotate_fortyfive(direction, orientation) {// orientation = 1 means clockwise, =-1 means counterclock
 	//find index in list that your direction sits
-	found_direction = false
-	index = 0
+	let found_direction = false
+	let index = 0
 	while (!found_direction) {
 		if (arraysEqual(direction_list[index], direction) || index ==7) {
 			found_direction = true
@@ -100,70 +131,115 @@ function rotate_fortyfive(direction, orientation) {// orientation = 1 means cloc
 		else {return [-1,0]}
 	} 
 }
-
+function transform_coords(x,y,direction) {//second version of transform coords, for toroidal mapping
+if (x==-1){ 
+	x=boardSize-1
+}
+if (x==boardSize) {
+	x=0
+}
+if (y == boardSize){ 
+	y=0
+}
+if (y==-1){  
+	y=boardSize-1
+}
+	return [x,y,direction]
+}
+ 
+function drawSquare(x,y,color){
+	ctx.beginPath()
+		ctx.fillStyle = color
+		ctx.fillRect(x*tileSize,y*tileSize,tileSize,tileSize)
+		ctx.stroke(); 
+}  
+ 
+function explosion_animation(coords) {
+	//console.log(typeof(direction_list[0][0]))
+	//console.log('poopyshoe')
+	//console.log(coords[0])
+	for (let i = 0; i < direction_list.length; i++){
+		drawSquare(coords[0]+direction_list[i][0],coords[1]+direction_list[i][1],'red')
+	}
+	drawSquare(coords[0],coords[1],"orange")
+} 
 
 // Client-side function to update player data in HTML element
 function updatePlayerData(playerData) {
   const playerElement = document.getElementById("playerData");
-
+	let playerName = 'noPlayer'
+	if (playerData.id == 'r') {
+		playerName = 'Red'
+	}
+	else (
+		playerName = 'Blue'
+	)
   // Update the HTML element with player data
-  playerElement.innerHTML = `
-    <p>ID: ${playerData.id}</p>
-    <p>Name: ${playerData.name}</p>
+  playerElement.innerHTML = ` 
+    <p>Your color: ${playerName}</p>
     <p>Ships: ${playerData.shipCount}</p>
-    <p>Level: ${playerData.level}</p>
   `;
 }
 socket.on("playerData", function(playerData) {
   // Call the updatePlayerData function to update the HTML element
   updatePlayerData(playerData);
 });
-
-
-function render_piece(x,y,direction) {
+ 
+var clock_tilt,counter_tilt;
+function render_piece(x,y,direction, color = 'black') {
 	if (Math.abs(direction[0]) + Math.abs(direction[1]) == 1) {
 		clock_tilt = rotate_fortyfive(rotate_fortyfive(direction,1),1);
 		counter_tilt = rotate_fortyfive(rotate_fortyfive(direction,-1),-1);
-	}
-	else {
+	} 
+	else { 
 		clock_tilt = rotate_fortyfive(direction,1);
 		counter_tilt = rotate_fortyfive(direction,-1);
 	}
+	//console.log(clock_tilt)
 	ctx.beginPath();
+	ctx.strokeStyle = color
 	ctx.moveTo((x-.5)*tileSize+(-.5*direction[0])*tileSize*.5,(y-.5)*tileSize+(-.5*direction[1])*tileSize*.5)
 	ctx.lineTo((x-.5)*tileSize+(.5*direction[0])*tileSize*.5,(y-.5)*tileSize+(.5*direction[1])*tileSize*.5)
 	ctx.moveTo((x-.5)*tileSize+(.5*clock_tilt[0])*tileSize*.5,(y-.5)*tileSize+(.5*clock_tilt[1])*tileSize*.5)
 	ctx.lineTo((x-.5)*tileSize+(.5*direction[0])*tileSize*.5,(y-.5)*tileSize+(.5*direction[1])*tileSize*.5)
 	ctx.moveTo((x-.5)*tileSize+(.5*counter_tilt[0])*tileSize*.5,(y-.5)*tileSize+(.5*counter_tilt[1])*tileSize*.5)
 	ctx.lineTo((x-.5)*tileSize+(.5*direction[0])*tileSize*.5,(y-.5)*tileSize+(.5*direction[1])*tileSize*.5)
-	ctx.srokeStyle = 'black'
 	ctx.lineWidth = 2
 	ctx.stroke();
 }
 
 // Draw the game board
-function renderBoard(redList,blueList,placeList) {
+const playerIDtoColorMap = {'r':'red','b':'blue'}
+function renderBoard(placeList) {
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
 	for(let y = 0; y < boardSize; y++) { 
 		for(let x = 0; x < boardSize; x++) {
-			if (gameBoard[x][y] == 'r') {
-				ctx.font = "20px serif"
-				ctx.fillText('r',(x+.4)*tileSize,(y+.6)*tileSize)
-				console.log("drawing board")
-			}
-			if (gameBoard[x][y] == 'b') {
-				ctx.font = "20px serif"
-				ctx.fillText('b',(x+.4)*tileSize,(y+.6)*tileSize)
-				console.log("drawing board")
-			}
-			
 			ctx.strokeStyle = 'black';
 			ctx.strokeRect(x * tileSize, y * tileSize, tileSize, tileSize);
 		}
 	}
+	if (placeList != undefined) {
+	for (let i = 0; i < placeList.length; i++) {
+		let renderColor = playerIDtoColorMap[placeList[i][2]]
+		//console.log(placeList[0][0])
+		render_piece(placeList[i][0][0]+1,placeList[i][0][1]+1,placeList[i][1],playerIDtoColorMap[placeList[i][2]])
+	}} 
 }
-renderBoard()
+renderBoard() 
 
+function remove_selection_icon() {
+	for (let locale in place_list) {
+		fill_over_selection_square(place_list[locale][0],place_list[locale][1])
+	}
+} 
+ 
+function fill_over_selection_square(x,y) {
+			ctx.beginPath(); 
+			ctx.fillStyle = "green";
+			ctx.fillRect(x*tileSize + tileSize*.1,y*tileSize + tileSize*.1,tileSize*.8,tileSize*.8);
+			ctx.stroke();
+}
+ 
 // function locate_ships() {
 	// pieces_list = []
 	// for (let i = 0; i < boardSize; i++) {
@@ -182,20 +258,20 @@ renderBoard()
 	
 // }
 
-var place_ship = false;
+var place_ship = true;
 var rotate_piece = false;
+var retract_ship = false;
 var place_list = []
 var focus_location = []
 function display_selection_tiles(tiles) {
+	console.log(tiles); 
 	place_list = tiles
-	for (let i = 0; i < tiles.length; i++) {
-		ctx.beginPath()
-		ctx.fillStyle = "green"
-		ctx.fillRect(tiles[i][0]*tileSize,tiles[i][1]*tileSize,tileSize,tileSize)
-		ctx.stroke();
+	for (let i = 0; i < tiles.length; i++) { 
+		drawSquare(tiles[i][0],tiles[i][1],"green")
 	}
 }
-display_selection_tiles([[1,1]])
+//display_selection_tiles([[1,1],[2,5]])
+//var tiles = [[1,1],[2,5]]
 canvas.addEventListener('click', function(event) {
 	// Calculate the clicked tile's coordinates
 	const rect = canvas.getBoundingClientRect();
@@ -203,11 +279,17 @@ canvas.addEventListener('click', function(event) {
 	const y = event.clientY - rect.top;
 	const tileX = Math.floor(x / tileSize);
 	const tileY = Math.floor(y / tileSize);
-	if (place_ship && tiles.includes([x,y])){
-		render_piece(x,y,[1,1])
+	if (place_ship && place_list.some(tile => tile[0] === tileX && tile[1] === tileY)){
+		remove_selection_icon()
+		render_piece(tileX+1,tileY+1,[1,1])  
+		//console.log('here')
+		
 		rotate_piece = true
-		focus_location = [x,y,[1,1]]
+		focus_location = [tileX,tileY,[1,1]]
 	}
+	if (retract_ship) {
+		return;  
+	}; 
 	socket.emit('cellClicked', [tileX, tileY])
 	// Update and log the game state
 	// gameBoard[tileY][tileX] = 'X';
@@ -217,29 +299,50 @@ canvas.addEventListener('click', function(event) {
 	// ctx.fillStyle = 'rgba(0, 0, 255, 0.3)';
 	// ctx.fillRect(tileX * tileSize, tileY * tileSize, tileSize, tileSize);
 });
-canvas.addEventListener('keydown', function(event) {
-    switch (event.key) {
-    case "ArrowLeft":
-        if (rotate_piece) {
-			ctx.beginPath[]
+
+function respondToArrows(rotate_orientation) {
+	//console.log('ehleel')
+	if (rotate_piece) {
+			remove_selection_icon()
+			focus_location = [focus_location[0],focus_location[1],rotate_fortyfive(focus_location[2],rotate_orientation)];
+			render_piece(focus_location[0]+1,focus_location[1]+1,focus_location[2]);
 		}
-    case "ArrowRight":
-        // Right pressed
-        break;
-}
+} 
+window.addEventListener('keydown', function(event) {
+
+    if (event.key == "a") { 
+		//console.log('aaa')
+        respondToArrows(-1);
+	}
+	if (event.key == "d") {
+        respondToArrows(1);
+	}
+	if (event.key == 'Enter'&& place_ship) {
+		place_ship = false;
+		rotate_piece = false;
+		
+		socket.emit('ship_placed',[focus_location[0],focus_location[1],focus_location[2]]);
+	}
 });
 
-
-
-socket.on('placeShip', (player_moves) => {//this will signal the player client to update the board, display the board events and then show where the player can place their troops. 
-	place_ship = true
-	display_selection_tiles(player_moves)
+socket.on('explosion',(explosion_coords)=> {
+	explosion_animation(explosion_coords);
 })
 
+socket.on('renderBoard', (player_moves) => {//this will signal the player client to update the board, display the board events and then show where the player can place their troops. 
+	//console.log(player_moves)
+	renderBoard(player_moves)
+})
 
+socket.on('place_ship', (data)=>{
+	console.log(data)
+	display_selection_tiles(data)
+	place_ship = true
+	
+})
 
 socket.on('move', (elementData) => {
-	console.log(elementData)
+	//console.log(elementData)
   gameBoard[elementData.locationX][elementData.locationY] = elementData.player
   renderBoard()
 });
--- server.js
+++ server.js
@@ -8,12 +8,13 @@ const io = socketIO(server);
 
 app.use(express.static(__dirname));
 
-const tileSize = 50;
-const boardSize = 8;
+const tileSize = 32; 
+const boardSize = 8; 
 const boardPixelSize = tileSize *  boardSize;
+const gameSpeed = 666;
+
 // Initialize the game board as a 2D array
 	const gameBoard = new Array(boardSize).fill(null).map(() => new Array(boardSize).fill(null));
-
 
 // const wins_list = [[1,4,7],[0,3,6], [2,5,8],[0,1,2],[3,4,5],[6,7,8],[0,4,8],[4,2,6]]
 // function check_win() {
@@ -32,85 +33,254 @@ playerCount = 0
 currentPlayer = 'r'
 const templateNewBaord = new Array(boardSize).fill(null).map(() => new Array(boardSize).fill(null));
 
+function searchForArray(haystack, needle){
+  var i, j, current;
+  for(i = 0; i < haystack.length; ++i){
+    if(needle.length === haystack[i].length){
+      current = haystack[i];
+      for(j = 0; j < needle.length && needle[j] === current[j]; ++j);
+      if(j === needle.length)
+        return i;
+    }
+  }
+  return -1;
+}
+
+function arraysEqual(a, b) {
+  if (a === b) return true;
+  if (a == null || b == null) return false;
+  if (a.length !== b.length) return false;
+  for (var i = 0; i < a.length; ++i) {
+    if (a[i] !== b[i]) return false;
+  }
+  return true;
+}
 
 
 function rotate_counter(direction) {//rotate cardinal direction counter clockwise two 'notches', aka 90 degrees,aka compass pieces of 8
 	newY = direction[0]*-1
 	newX = direction[1]
-	return [newX,newY]
+	return [newX,newY] 
 }
 function rotate_clock(direction) {//ditto clockwise
 	newX = direction[1]*-1
-	newY = direction[0]
+	newY = direction[0] 
 	return [newX,newY]
 }
  
 //if a ship's new location after the update is within the 8x8 board, then this will return the the parameters unchanged
-function transform_coords(x,y,direction) {//otherwise this will map ships that have 'fallen off the edge of the board' back onto the board and update the direction, returning new coordinates and direction. 
-if (x==-1&&y==boardSize){ 
-	x=0
-	y=boardSize-1
-	direction = [1,-1]
-}
-if (x==boardSize&&y==-1) {
-	x=boardSize-1
-	y=0
-	direction = [-1,1]
-}
-if ((x+y)>boardSize*2-2){ 
-	x=0
-	y=0
-	direction = [-1,-1]
-}
-if (x+y==-2){ 
-	x = 0
-	y=0
-	direction = [1,1]
-}
-	if (x==boardSize) {
-		x=y;
-		y=0
-		direction = rotate_counter(direction)
-	}
-	if (y==boardSize) {
-		y=x;
-		x=0
-		direction = rotate_clock(direction)
+// function transform_coords(x,y,direction) {//otherwise this will map ships that have 'fallen off the edge of the board' back onto the board and update the direction, returning new coordinates and direction. 
+// if (x==-1&&y==boardSize){ 
+	// x=0
+	// y=boardSize-1
+	// direction = [1,-1]
+// }
+// if (x==boardSize&&y==-1) {
+	// x=boardSize-1
+	// y=0
+	// direction = [-1,1]
+// }
+// if ((x+y)>boardSize*2-2){ 
+	// x=0
+	// y=0
+	// direction = [-1,-1]
+// }
+// if (x+y==-2){ 
+	// x = 0
+	// y=0
+	// direction = [1,1]
+// }
+	// if (x==boardSize) {
+		// x=y;
+		// y=0
+		// direction = rotate_counter(direction)
+	// }
+	// if (y==boardSize) {
+		// y=x;
+		// x=0
+		// direction = rotate_clock(direction)
 		
-	}
-	if (x==-1) {
-		x=y
-		y=boardSize-1
-		direction = rotate_counter(direction)
-	}
-	if (y==-1) {
-		y=x
-		x=boardSize-1
-		direction = rotate_clock(direction)
-	}
+	// }
+	// if (x==-1) {
+		// x=y
+		// y=boardSize-1
+		// direction = rotate_counter(direction)
+	// }
+	// if (y==-1) {
+		// y=x
+		// x=boardSize-1
+		// direction = rotate_clock(direction)
+	// }
+	// return [x,y,direction]
+// }
+function transform_coords(x,y,direction) {//second version of transform coords, for toroidal mapping
+if (x==-1){ 
+	x=boardSize-1
+}
+if (x==boardSize) {
+	x=0
+}
+if (y == boardSize){ 
+	y=0
+}
+if (y==-1){ 
+	y=boardSize-1
+}
 	return [x,y,direction]
 }
 
+function check_for_collision() {
+	let collision_explode = []
+	let collision_same = []
+	for (let i = 0; i < globalShipList.length; i++) {
+		for (let j = i+1; j < globalShipList.length; j++) {
+			if (globalShipList[i].x == globalShipList[j].x && globalShipList[j].y == globalShipList[i].y) {
+				if (globalShipList[i].player == globalShipList[j].player) {
+					collision_same.push([globalShipList[i].x,globalShipList[i].y])
+				}
+				else {
+					collision_explode.push([globalShipList[i].x,globalShipList[i].y])
+				}
+			}
+		}
+	}
+	return {hetero:collision_explode,homo:collision_same}
+}
 
 // function update_board() {
 	// for (let i = 0; i < 
 
 // Server-side function to initialize player data
+var playerList = []
 function initializePlayerData(playerId) {
   // Simulating server-side data retrieval
   const playerData = {
     id: playerId,
-    name: "Player " + playerId,
-    shipCount: 0,
-    level: 1,
+    shipCount: 20,
 	shipList: [] 
   };
+  playerList.push([playerId,playerData])
   
   return playerData;
 }
 
+var explosion_signal_index = 0
+var explosion_list_holder = []
+function signal_explosion_list() {
+	console.log(`explosion list:${explosion_list_holder}`)
+	setTimeout(() => {
+		io.emit('explosion',explosion_list_holder[explosion_signal_index]);
+		if (explosion_signal_index > 0){
+			explosion_signal_index-=1;
+			signal_explosion_list();
+			}
+	},2*gameSpeed)
+}
+const direction_list = [[0,0],[-1,-1],[0,-1],[1,-1],[1,0],[1,1],[0,1],[-1,1],[-1,0]]
 
+function post_update() {
+	setTimeout(() => {
+				io.emit('renderBoard',convertShips())
+				},3*gameSpeed);
+			setTimeout(() => {
+			for (let key in playerMapping) {
+				//console.log(playerMapping[key])
+				placementdata = generate_moves(playerMapping[key])
+				//console.log(placementdata)
+				io.to(key).emit('place_ship', placementdata)
+			}
+			},3*gameSpeed);
+}
 
+function update_ships() {
+	for (let ship in globalShipList) {
+		globalShipList[ship].move() 
+	}
+	let hits = check_for_collision()
+	let explosion_list_holder = hits.hetero
+	let self_collide = hits.homo
+	setTimeout(()=>{
+	io.emit('renderBoard',convertShips())
+	if (!arraysEqual(hits.hetero,[])) {
+		explosion_signal_index = hits.hetero.length-1
+		
+		signal_explosion_list()
+		setTimeout(() => {
+			let remove_list = []
+			for (let hit in explosion_list_holder) {
+				for (let dir in direction_list) {
+					let new_pos = transform_coords(explosion_list_holder[hit][0]+direction_list[dir][0],explosion_list_holder[hit][1]+direction_list[dir][1],direction_list[dir])
+					console.log(new_pos)
+					for (let ship in globalShipList) {
+						if (globalShipList[ship].x == new_pos[0] && globalShipList[ship].y == new_pos[1]) {
+							//globalShipList.splice(ship,1);
+							remove_list.push(globalShipList[ship].index)
+						}	 
+					}
+				}  
+			}  
+			let dex = 0
+			//console.log(`removal list = ${remove_list}`)
+			while (dex < globalShipList.length) {
+				//console.log(globalShipList)
+				if (remove_list.includes(globalShipList[dex].index)) {
+					globalShipList.splice(dex, 1);
+					//console.log('now here')
+				} 
+				else {dex+=1}
+				}
+			io.emit('renderBoard', convertShips())
+			setTimeout(() => {
+				io.emit('renderBoard',convertShips())
+				},3*gameSpeed);
+			setTimeout(() => {
+			for (let key in playerMapping) {
+				console.log(playerMapping[key])
+				placementdata = generate_moves(playerMapping[key])
+				//console.log(placementdata)
+				io.to(key).emit('place_ship', placementdata)
+			}
+			},3*gameSpeed);
+		},4*gameSpeed*hits.hetero.length+2*gameSpeed) 
+	}
+	else {
+		post_update()
+	}
+	
+	if (!arraysEqual(hits.homo,[])) {
+		console.log('self hit')
+		remove_list = []
+		for (let hit in self_collide) {
+		for (let ship in globalShipList) {
+			if (globalShipList[ship].x == self_collide[hit][0] && globalShipList[ship].y == self_collide[hit][1]) {
+				remove_list.push(globalShipList[ship].index)
+				console.log('added to remove list')
+			}	 
+		}
+		}
+		let dex = 0
+			//console.log(`removal list = ${remove_list}`)
+			while (dex < globalShipList.length) {
+				//console.log(globalShipList)
+				if (remove_list.includes(globalShipList[dex].index)) {
+					for (let spot in playerList) {
+						if (playerList[spot][0] == globalShipList[dex].player) {
+							playerList[spot][1].shipCount +=1
+						}
+					}
+					globalShipList.splice(dex, 1);
+					//console.log('now here')
+					
+				} 
+				else {dex+=1}
+				}
+			
+	}
+	
+	},3*gameSpeed) 
+	
+} 
+ 
 class Ship {
 	static ship_index = 0;
 	static ship_list = [];
@@ -119,21 +289,22 @@ class Ship {
 		this.index = Ship.ship_index++;
 		Ship.ship_list.push(this)
 	}
-	place(x,y,direction) {
+	place(x,y,direction) { 
+		globalShipList.push(this)
 		this.direction = direction
 		this.x = x
 		this.y=y
 	}
 	move() {
-		this.previousX = x
-		this.previousY = y
+		this.previousX = this.x
+		this.previousY = this.y
 		this.x +=this.direction[0]
 		this.y +=this.direction[1]
 		let temp = transform_coords(this.x,this.y,this.direction)
 		this.x = temp[0]
 		this.y = temp[1]
 		this.direction = temp[2]
-	}
+	} 
 	destroy() {
 		for (let i = 0; i < ship_list.lenth; i++) {
 			if (this.index === ship_list[i].index) {
@@ -142,49 +313,127 @@ class Ship {
 		}
 	}
 }
-function generate_moves(player_id) {
-	let list = Ship.ship_list
-	let moves = []
-	for (let i = 0; i < list; i++) {
-		if (list[i].player = player_id && [list[i].x,list[i].y] != [list[i].previousX,list[i].previousY]) {
-			moves.push([list[i].previousX,list[i].previousY])
-		}
+
+
+function check_if_location_has_ship(cx,cy) {
+	let emptyChecker = true
+	for (let j = 0; j < globalShipList.length; j++) {
+		if (globalShipList[j].x == cx && globalShipList[j].y == cy) {
+			emptyChecker = false
+		} 
 	}
-	return moves
+	return emptyChecker;
 }
 
+function generate_moves(player_id) {
+	let list = globalShipList
+	let moves = []
+	//console.log(`ship list ${globalShipList} `)
+	for (let i = 0; i < list.length; i++) {
+		let ship = list[i]
+		//console.log(`checking if at ${ship.previousX},${ship.previousY} there is a ship ${check_if_location_has_ship(ship.previousX,ship.previousY)}`)
+		if ((ship.player == player_id) && 
+			//[ship.x,ship.y] != [ship.previousX,ship.previousY]&&
+			check_if_location_has_ship(ship.previousX,ship.previousY)) {
+			moves.push([ship.previousX,ship.previousY])}
+	} 
+	return moves
+}
+function convertShips() {
+	emitCoords = []
+		for (let i = 0; i < globalShipList.length; i++) {
+			emitCoords.push([[globalShipList[i].x,globalShipList[i].y],globalShipList[i].direction,globalShipList[i].player])
+		}
+		return emitCoords
+} 
 var waiting_for_move = true;
 var waiting_for_recover = false;
 var gameState = true
 var globalShipList = []
+var gameStart = false
+var placementCount = 0
+
 io.on('connection', (socket) => {
+	console.log(socket.id)
   if (playerCount < 2) {
     playerMapping[socket.id] = playerCount === 0 ? 'r' : 'b';
-    playerCount++;
+    playerCount++; 
   } else {
     socket.emit('error', 'The game is full. Please try again later.');
     return;
   }//this code assign
+  //console.log()   
   const playerData = initializePlayerData(playerMapping[socket.id]);
   socket.emit("playerData", playerData);
   socket.emit('boardState', { board: gameBoard, player: playerMapping[socket.id] });
-
-	socket.on('ship_placed', (data) => {
-		if (waiting_for_move) {
-		x=data[0]
-		y=data[1]
-		direction = data[2]
-		console.log(`x:${x} y:${y}`)
-		let moves_list = generate_moves(socket.id)
-		if (moves_list.includes([x,y])) {
-		  let temp_ship = new Ship(socket.id)
-		  temp_ship.place(x,y,)
-		  io.emit('move', { locationX: x, locationY: y, player: currentPlayer});
-		  currentPlayer = currentPlayer === 'r' ? 'b' : 'r';
-		  waiting_for_move = false;
-	  }
+	 
+	if (playerCount == 2 && !gameStart) {
+		//console.log('two players connected')
+		gameStart = true
+		redStart = new Ship(playerMapping[Object.keys(playerMapping)[0]])
+		redStart.place(1,1,[1,0])
+		//console.log(playerMapping[Object.keys(playerMapping)[1]])
+		blueStart = new Ship(playerMapping[Object.keys(playerMapping)[1]])
+		blueStart.place(6,6,[-1,0])
+		blueStart.move()
+		// blue2Start = new Ship(playerMapping[Object.keys(playerMapping)[1]])
+		// blue2Start.place(2,6,[1,0])
+		// blue2Start.move()
+		redStart.move() 
+		//console.log(Object.keys(playerMapping))
+		for (let key in playerMapping) {
+			//console.log(`sending ship data ${convertShips()}`) 
+			io.to(key).emit('renderBoard', convertShips())
+			}
+		setTimeout(() => {
+			for (let key in playerMapping) {
+				//console.log(playerMapping[key])
+				placementdata = generate_moves(playerMapping[key])
+				//console.log(globalShipList)
+				io.to(key).emit('place_ship', placementdata)
+		}
+		},3*gameSpeed);
 	}
-	}); 
+	
+	
+	socket.on('ship_placed', (data) => {
+		if (data != 'no ships') {
+		
+		//console.log('you did place a ship')
+		if (waiting_for_move) {
+		x=data[0]   
+		y=data[1]   
+		direction = data[2] 
+		//console.log(`x:${x} y:${y}`)
+		let moves_list = generate_moves(socket.id)
+		
+		if (searchForArray([x,y],moves_list)) {  
+		  let temp_ship = new Ship(playerMapping[socket.id])
+		  temp_ship.place(x,y,direction)
+		  //io.emit('move', { locationX: x, locationY: y, player: currentPlayer});
+		  //currentPlayer = currentPlayer === 'r' ? 'b' : 'r';
+		  //waiting_for_move = false;
+		  placementCount+=1;
+		  playerData.shipCount -= 1
+		  socket.emit("playerData", playerData);
+		  //console.log('hit the movelist check'); 
+		}
+		}
+		else {placementCount+=1
+			
+		}
+		
+		if (placementCount >1) {
+			//console.log('bothPlaced')
+			placementCount = 0
+			io.emit('renderBoard', convertShips())
+			update_ships()
+			socket.emit('playerData', playerData)
+		} 
+	} 
+	});
+	
+	
 
   
 
@@ -197,5 +446,5 @@ io.on('connection', (socket) => {
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, '0.0.0.0', () => {
   console.log(`Server is running on port ${PORT}`);
-});
+}); 
 
